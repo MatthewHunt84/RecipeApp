dc8971294b67a3821b4911abfc5d57468ecf9d88
Matt Hunt <matthew.hunt@wundermanthompson.com>
2025-02-17 08:23:43 -0700

    Create URLSessionHTTPClient test file

---
+++ FetchRecipeTests/Recipe API/URLSessionHTTPClient.swift
@@ -0,0 +1,16 @@
+//
+//  URLSessionHTTPClient.swift
+//  FetchRecipeTests
+//
+//  Created by Matt Hunt on 2/17/25.
+//
+
+import Testing
+
+struct URLSessionHTTPClient {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
