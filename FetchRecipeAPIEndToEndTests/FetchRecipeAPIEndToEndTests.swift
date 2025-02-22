cb78a1b73b21352e416e6813380306583bd22327
Matt Hunt <matthew.hunt@wundermanthompson.com>
2025-02-21 22:49:00 -0700

    Created helper methods to confirm recipes match expected results for specific data

--- FetchRecipeAPIEndToEndTests/FetchRecipeAPIEndToEndTests.swift
+++ FetchRecipeAPIEndToEndTests/FetchRecipeAPIEndToEndTests.swift
@@ -29,26 +29,56 @@ struct FetchRecipeAPIEndToEndTests {
         
         let recipes = try await loader.load()
         
-        let expectedFirstRecipe = Recipe(
-            cuisine: "Malaysian",
-            name: "Apam Balik",
-            photoUrlLarge: Optional("https://d3jbb8n5wk0qxi.cloudfront.net/photos/b9ab0071-b281-4bee-b361-ec340d405320/large.jpg"),
-            photoUrlSmall: Optional("https://d3jbb8n5wk0qxi.cloudfront.net/photos/b9ab0071-b281-4bee-b361-ec340d405320/small.jpg"),
-            uuid: "0c6ca6e7-e32a-4053-b824-1dbf749910d8",
-            sourceUrl: Optional("https://www.nyonyacooking.com/recipes/apam-balik~SJ5WuvsDf9WQ"),
-            youtubeUrl: Optional("https://www.youtube.com/watch?v=6R8ffRRJcrg"))
-        
-        let expectedSecondRecipe = Recipe(
-            cuisine: "British",
-            name: "Apple & Blackberry Crumble",
-            photoUrlLarge: Optional("https://d3jbb8n5wk0qxi.cloudfront.net/photos/535dfe4e-5d61-4db6-ba8f-7a27b1214f5d/large.jpg"),
-            photoUrlSmall: Optional("https://d3jbb8n5wk0qxi.cloudfront.net/photos/535dfe4e-5d61-4db6-ba8f-7a27b1214f5d/small.jpg"),
-            uuid: "599344f4-3c5c-4cca-b914-2210e3b3312f",
-            sourceUrl: Optional("https://www.bbcgoodfood.com/recipes/778642/apple-and-blackberry-crumble"),
-            youtubeUrl: Optional("https://www.youtube.com/watch?v=4vhcOwVBDO4"))
-        
-        #expect(recipes.first == expectedFirstRecipe)
-        #expect(recipes[1] == expectedSecondRecipe)
+        #expect(recipes[0] == expectedRecipe(at: 0))
+        #expect(recipes[1] == expectedRecipe(at: 1))
+    }
+    
+    
+    // MARK: Helpers
+    
+    func expectedRecipe(at index: Int) -> Recipe {
+        Recipe(cuisine: cuisine(at: index),
+               name: name(at: index),
+               photoUrlLarge: photoURLLarge(at: index),
+               photoUrlSmall: photoURLSmall(at: index),
+               uuid: uuid(at: index),
+               sourceUrl: sourceURL(at: index),
+               youtubeUrl: youtubeURL(at: index))
+    }
+    
+    func cuisine(at index: Int) -> String {
+        let cuisines = ["Malaysian", "British"]
+        return cuisines[index]
+    }
+    
+    func name(at index: Int) -> String {
+        let names = ["Apam Balik", "Apple & Blackberry Crumble"]
+        return names[index]
+    }
+    
+    func photoURLLarge(at index: Int) -> String? {
+        let photos = ["https://d3jbb8n5wk0qxi.cloudfront.net/photos/b9ab0071-b281-4bee-b361-ec340d405320/large.jpg", "https://d3jbb8n5wk0qxi.cloudfront.net/photos/535dfe4e-5d61-4db6-ba8f-7a27b1214f5d/large.jpg"]
+        return photos[index]
+    }
+    
+    func photoURLSmall(at index: Int) -> String? {
+        let photos = ["https://d3jbb8n5wk0qxi.cloudfront.net/photos/b9ab0071-b281-4bee-b361-ec340d405320/small.jpg", "https://d3jbb8n5wk0qxi.cloudfront.net/photos/535dfe4e-5d61-4db6-ba8f-7a27b1214f5d/small.jpg"]
+        return photos[index]
+    }
+    
+    func uuid(at index: Int) -> String {
+        let uuids = ["0c6ca6e7-e32a-4053-b824-1dbf749910d8", "599344f4-3c5c-4cca-b914-2210e3b3312f"]
+        return uuids[index]
+    }
+    
+    func sourceURL(at index: Int) -> String? {
+        let sourceURLs = ["https://www.nyonyacooking.com/recipes/apam-balik~SJ5WuvsDf9WQ", "https://www.bbcgoodfood.com/recipes/778642/apple-and-blackberry-crumble"]
+        return sourceURLs[index]
+    }
+    
+    func youtubeURL(at index: Int) -> String? {
+        let youtubeURLs = ["https://www.youtube.com/watch?v=6R8ffRRJcrg", "https://www.youtube.com/watch?v=4vhcOwVBDO4"]
+        return youtubeURLs[index]
     }
 
 }
