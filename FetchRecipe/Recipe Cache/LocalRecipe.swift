cc4bab3e824d30e6c53b968636c6663994a2966d
Matt Hunt <matthew.hunt@wundermanthompson.com>
2025-02-22 17:34:20 -0700

    Moved LocalRecipe to its own file

---
+++ FetchRecipe/Recipe Cache/LocalRecipe.swift
@@ -0,0 +1,29 @@
+//
+//  Untitled.swift
+//  FetchRecipe
+//
+//  Created by Matt Hunt on 2/22/25.
+//
+
+import Foundation
+
+public struct LocalRecipe: Codable, Equatable {
+    public let cuisine: String
+    public let name: String
+    public let photoUrlLarge: String?
+    public let photoUrlSmall: String?
+    public let uuid: String
+    public let sourceUrl: String?
+    public let youtubeUrl: String?
+    
+    public init(cuisine: String, name: String, photoUrlLarge: String?, photoUrlSmall: String?, uuid: String, sourceUrl: String?, youtubeUrl: String?) {
+        self.cuisine = cuisine
+        self.name = name
+        self.photoUrlLarge = photoUrlLarge
+        self.photoUrlSmall = photoUrlSmall
+        self.uuid = uuid
+        self.sourceUrl = sourceUrl
+        self.youtubeUrl = youtubeUrl
+    }
+}
+
