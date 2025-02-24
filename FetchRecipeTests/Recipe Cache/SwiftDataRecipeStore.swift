d0fa2b8864e87c571371f45cc6a3eb0191b48717
Matt Hunt <matthew.hunt@wundermanthompson.com>
2025-02-24 07:47:47 -0700

    Enabled Swift 6 language mode

---
+++ FetchRecipeTests/Recipe Cache/SwiftDataRecipeStore.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftDataRecipeStore.swift
+//  FetchRecipeTests
+//
+//  Created by Matt Hunt on 2/24/25.
+//
+
+import Testing
+
+struct SwiftDataRecipeStore {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
