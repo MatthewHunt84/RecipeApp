04c96f05e388effb3c28e6b0cc3cb95d05031e7c
Matt Hunt <matthew.hunt@wundermanthompson.com>
2025-03-05 07:49:29 -0700

    insertion to SwfitData on disk storage returns expected Recipes

--- FetchRecipeIntegrationTests/FetchRecipeIntegrationTests.swift
+++ FetchRecipeIntegrationTests/FetchRecipeIntegrationTests.swift
@@ -7,6 +7,7 @@
 
 import Testing
 import FetchRecipe
+import Foundation
 import SwiftData
 
 @Suite(.serialized)
@@ -20,10 +21,37 @@ struct FetchRecipeIntegrationTests {
         #expect(recipes.count == 0)
     }
 
+    @Test func insertion_withEmptyCache_shouldAddRecipesToCache() async throws {
+        let sutToPerformSave = await makeSUT()
+        let sutToPerformLoad = await makeSUT()
+        let insertedRecipes = makeLocalRecipes()
+        
+        try await sutToPerformSave.insertRecipes(insertedRecipes)
+        let retrievedRecipes = try await sutToPerformLoad.retrieveRecipes()
+        
+        #expect(retrievedRecipes.sorted() == insertedRecipes.sorted())
+    }
+    
     func makeSUT() async -> SwiftDataStore {
         let container = try! ModelContainer(for: SwiftDataLocalRecipe.self, configurations: ModelConfiguration(isStoredInMemoryOnly: false))
         let sut = SwiftDataStore(modelContainer: container)
         try! await #require(try sut.deleteCachedRecipes())
+        
         return sut
+    }
+    
+    func makeLocalRecipe() -> LocalRecipe {
+        LocalRecipe(
+            cuisine: "Any",
+            name: "Any",
+            photoUrlLarge: nil,
+            photoUrlSmall: nil,
+            uuid: UUID().uuidString,
+            sourceUrl: nil,
+            youtubeUrl: nil)
+    }
+    
+    func makeLocalRecipes() -> [LocalRecipe] {
+        (0..<Int.random(in: 1...10)).map { _ in makeLocalRecipe() }
     }
 }
