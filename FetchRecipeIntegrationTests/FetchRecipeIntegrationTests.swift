25109d7b29ed3986138556e8675d5eb2dd9b142a
Matt Hunt <matthew.hunt@wundermanthompson.com>
2025-03-05 06:43:56 -0700

    Created integration test target

---
+++ FetchRecipeIntegrationTests/FetchRecipeIntegrationTests.swift
@@ -0,0 +1,16 @@
+//
+//  FetchRecipeIntegrationTests.swift
+//  FetchRecipeIntegrationTests
+//
+//  Created by Matt Hunt on 3/5/25.
+//
+
+import Testing
+
+struct FetchRecipeIntegrationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
