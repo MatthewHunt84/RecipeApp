1d84a4a1de2fd4c83417c57ac3df5aac36d7ef02
Matt Hunt <matthew.hunt@wundermanthompson.com>
2025-03-05 07:11:06 -0700

    Test retrieving from empty swiftData store on disk returns empty recipe array

--- FetchRecipeIntegrationTests/FetchRecipeIntegrationTests.swift
+++ FetchRecipeIntegrationTests/FetchRecipeIntegrationTests.swift
@@ -6,11 +6,24 @@
 //
 
 import Testing
+import FetchRecipe
+import SwiftData
 
+@Suite(.serialized)
 struct FetchRecipeIntegrationTests {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    @Test func retrieve_withEmptyCache_shouldReturnEmptyRecipeArray() async throws {
+        let sut = await makeSUT()
+        
+        let recipes = try await sut.retrieveRecipes()
+
+        #expect(recipes.count == 0)
     }
 
+    func makeSUT() async -> SwiftDataStore {
+        let container = try! ModelContainer(for: SwiftDataLocalRecipe.self, configurations: ModelConfiguration(isStoredInMemoryOnly: false))
+        let sut = SwiftDataStore(modelContainer: container)
+        try! await #require(try sut.deleteCachedRecipes())
+        return sut
+    }
 }
