808d047a9d182ab539721c99b2c1873bc482158b
Matt Hunt <matthew.hunt@wundermanthompson.com>
2025-03-16 12:21:54 -0600

    Moved SwiftDataLocalRecipe into SwiftDataStore to allow for fileprivate access of image data variable

--- FetchRecipe/Recipe Cache/Infrastructure/SwiftDataStore.swift
+++ FetchRecipe/Recipe Cache/Infrastructure/SwiftDataStore.swift
@@ -11,14 +11,14 @@ import SwiftData
 @ModelActor
 public actor SwiftDataStore: RecipeStore {
     
-    public func retrieveRecipes() async throws -> [LocalRecipe] {
+    public func retrieveRecipes() throws -> [LocalRecipe] {
         let descriptor = FetchDescriptor<SwiftDataLocalRecipe>()
         let swiftDataModels: [SwiftDataLocalRecipe] = try modelContext.fetch(descriptor)
         return swiftDataModels.map { $0.local }
     }
     
-    public func insertRecipes(_ recipes: [LocalRecipe]) async throws {
-        try await deleteCachedRecipes()
+    public func insertRecipes(_ recipes: [LocalRecipe]) throws {
+        try deleteCachedRecipes()
         let swiftDataModels = recipes.map(SwiftDataLocalRecipe.init)
         swiftDataModels.forEach { model in
             modelContext.insert(model)
@@ -26,8 +26,100 @@ public actor SwiftDataStore: RecipeStore {
         try modelContext.save()
     }
     
-    public func deleteCachedRecipes() async throws {
+    public func deleteCachedRecipes() throws {
         try modelContext.delete(model: SwiftDataLocalRecipe.self)
         try modelContext.save()
+    }
+}
+
+extension SwiftDataStore: RecipeImageDataStore {
+    
+    public enum Error: Swift.Error {
+        case urlNotFound
+        case duplicateUrl
+    }
+    
+    public func insert(_ data: Data, for url: URL) throws {
+        let urlString = url.absoluteString
+        let predicate = #Predicate<SwiftDataLocalRecipe> { recipe in
+            recipe.photoUrlSmall == urlString
+        }
+        let descriptor = FetchDescriptor<SwiftDataLocalRecipe>(predicate: predicate)
+        let matchedRecipe = try modelContext.fetch(descriptor)
+        let recipes = try retrieveRecipes()
+        guard matchedRecipe.count < 2 else {
+            throw Error.duplicateUrl
+        }
+        if let recipe = matchedRecipe.first {
+            recipe.photoUrlSmallImageData = data
+            try modelContext.save()
+        } else {
+            throw Error.urlNotFound
+        }
+    }
+    
+    public func retrieveData(for url: URL) throws -> Data? {
+        let urlString = url.absoluteString
+        let predicate = #Predicate<SwiftDataLocalRecipe> { recipe in
+            recipe.photoUrlSmall == urlString
+        }
+        let descriptor = FetchDescriptor<SwiftDataLocalRecipe>(predicate: predicate)
+        let matchedRecipe = try modelContext.fetch(descriptor)
+        guard matchedRecipe.count < 2 else {
+            throw Error.duplicateUrl
+        }
+        guard let imageData = matchedRecipe.first?.photoUrlSmallImageData else {
+            throw Error.urlNotFound
+        }
+        return imageData
+    }
+}
+
+@Model
+public class SwiftDataLocalRecipe {
+    fileprivate var cuisine: String
+    fileprivate var name: String
+    fileprivate var photoUrlLarge: String?
+    fileprivate var photoUrlSmall: String?
+    @Attribute(.unique) fileprivate var uuid: String
+    fileprivate var sourceUrl: String?
+    fileprivate var youtubeUrl: String?
+    fileprivate var photoUrlSmallImageData: Data?
+    
+    public init(cuisine: String,
+         name: String,
+         photoUrlLarge: String?,
+         photoUrlSmall: String?,
+         uuid: String,
+         sourceUrl: String?,
+         youtubeUrl: String?) {
+        self.cuisine = cuisine
+        self.name = name
+        self.photoUrlLarge = photoUrlLarge
+        self.photoUrlSmall = photoUrlSmall
+        self.uuid = uuid
+        self.sourceUrl = sourceUrl
+        self.youtubeUrl = youtubeUrl
+    }
+    
+    public var local: LocalRecipe {
+        LocalRecipe(
+            cuisine: cuisine,
+            name: name,
+            photoUrlLarge: photoUrlLarge,
+            photoUrlSmall: photoUrlSmall,
+            uuid: uuid,
+            sourceUrl: sourceUrl,
+            youtubeUrl: youtubeUrl)
+    }
+    
+    public init(_ localRecipe: LocalRecipe) {
+        self.cuisine = localRecipe.cuisine
+        self.name = localRecipe.name
+        self.photoUrlLarge = localRecipe.photoUrlLarge
+        self.photoUrlSmall = localRecipe.photoUrlSmall
+        self.uuid = localRecipe.uuid
+        self.sourceUrl = localRecipe.sourceUrl
+        self.youtubeUrl = localRecipe.youtubeUrl
     }
 }
