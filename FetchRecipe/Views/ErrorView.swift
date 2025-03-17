6893c15fdb348532bb11ae7d7f9364d8c0e305cc
Matt Hunt <matthew.hunt@wundermanthompson.com>
2025-03-17 10:48:03 -0600

    Created ErrorView for composition / model container failure during app launch with retry function

---
+++ FetchRecipe/Views/ErrorView.swift
@@ -0,0 +1,53 @@
+//
+//  ErrorView.swift
+//  FetchRecipe
+//
+//  Created by Matt Hunt on 3/17/25.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    let error: Error
+    let retryAction: () -> Void
+    
+    init(error: Error, retryAction: @escaping () -> Void) {
+        self.error = error
+        self.retryAction = retryAction
+    }
+    
+    var body: some View {
+        ZStack {
+            Color.blue.ignoresSafeArea()
+            Rectangle().fill(.blue.gradient).ignoresSafeArea()
+            
+            VStack(spacing: 20) {
+                Image(systemName: "exclamationmark.bubble")
+                    .font(.system(size: 60))
+                    .foregroundColor(.red)
+                
+                Text("D'oh!")
+                    .font(.title)
+                
+                Text(error.localizedDescription)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                
+                Button("Retry", systemImage: "arrow.clockwise", action: retryAction)
+                    .padding()
+                    .background(.green.gradient)
+                    .tint(.primary)
+                    .clipShape(.capsule)
+            }
+            .padding()
+        }
+        
+    }
+}
+
+
+#Preview {
+    ErrorView(error: NSError(domain: "loadError", code: 0)) {
+        print("Retry")
+    }
+}
